1adef835ee8fca4b722fa449335f73c9bd919173
Irikos <andrei.bardezi@gmail.com>
2018-06-05 18:19:12 +0300

    add first step of sessions form

--- FMI-Cazare/src/app/pages/session/SessionCtrl.js
+++ FMI-Cazare/src/app/pages/session/SessionCtrl.js
@@ -8,37 +8,7 @@
     var vm = this;
     $scope.dates;
     $scope.session = {};
-
-    $scope.session.dateStartOpen = openStart;
-    $scope.session.dateStartOpened = false;
-    function openStart() {
-      $scope.session.dateStartOpened = true;
-    }
-
-
-    $scope.options = {
-      showWeeks: false
-    };
-
-    $scope.format = 'dd.MM.yyyy';
-    $scope.options = {
-      showWeeks: false
-    };
-
-    $scope.dt = new Date();
-    $scope.options = {
-      showWeeks: false
-    };
-    $scope.open = open;
-    $scope.opened = false;
-    $scope.format = 'dd.MM.yyyy';
-    $scope.options = {
-      showWeeks: false
-    };
-
-    function open() {
-      $scope.opened = true;
-    }
+    $scope.session.startDate = null;
 
   };
 
