3853977eb5424baeb052962b7c862fd2ebcddd9e
Irikos <andrei.bardezi@gmail.com>
2018-06-27 17:06:19 +0300

    finish student form frontend

--- FMI-Cazare/src/app/pages/studentForm/StudentFormCtrl.js
+++ FMI-Cazare/src/app/pages/studentForm/StudentFormCtrl.js
@@ -4,7 +4,7 @@
   angular.module('FMI-Cazare.pages.studentForm')
     .controller('StudentFormCtrl', StudentFormCtrl);
 
-  function StudentFormCtrl($scope, toastr, editableOptions, editableThemes, baConfig) {
+  function StudentFormCtrl($scope, toastr, editableOptions, editableThemes, baConfig, $filter) {
     editableOptions.theme = 'bs3';
     editableThemes['bs3'].submitTpl = '<button type="submit" class="btn btn-primary btn-with-icon"><i class="ion-checkmark-round"></i></button>';
     editableThemes['bs3'].cancelTpl = '<button type="button" ng-click="$form.$cancel()" class="btn btn-default btn-with-icon"><i class="ion-close-round"></i></button>';
@@ -13,65 +13,92 @@
     $scope.test = "Hello, world!";
 
     $scope.student = {
-      "last_name": "Doe",
-      "first_name": "John",
+      "lastName": "Doe",
+      "firstName": "John",
       "cnp": "1900121765292",
-      "ic_serie": "TM",
-      "ic_number": "050949",
-      "birth_place": "Cluj-Napoca",
+      "icSerie": "TM",
+      "icNumber": "050949",
+      "birthPlace": "Cluj-Napoca",
       "address": "Magheru 4",
-      "father_first_name": "Vasile",
-      "mother_first_name": "Lenuta",
+      "fatherFirstName": "Vasile",
+      "motherFirstName": "Lenuta",
       "email": "john.doe@fmi.unibuc.ro",
       "specialization": "Informatică",
       "year": "2",
       "gender": "masculin",
       "role": "student",
-
+      "social": false,
+      "socialincome": "",
+      "continuity": false,
+      "lastDorm": ""
     };
-
-
 
     $scope.transparent = baConfig.theme.blur;
-    var dashboardColors = baConfig.colors.dashboard;
-    var colors = [];
-    for (var key in dashboardColors) {
-      colors.push(dashboardColors[key]);
-    }
 
-    function getRandomColor() {
-      var i = Math.floor(Math.random() * (colors.length - 1));
-      return colors[i];
-    }
 
-    $scope.todoList = [
-      { text: 'Check me out' },
-      { text: 'Lorem ipsum dolor sit amet, possit denique oportere at his, etiam corpora deseruisse te pro' },
-      { text: 'Ex has semper alterum, expetenda dignissim' },
-      { text: 'Vim an eius ocurreret abhorreant, id nam aeque persius ornatus.' },
-      { text: 'Simul erroribus ad usu' },
-      { text: 'Ei cum solet appareat, ex est graeci mediocritatem' },
-      { text: 'Get in touch with akveo team' },
-      { text: 'Write email to business cat' },
-      { text: 'Have fun with blur admin' },
-      { text: 'What do you think?' },
+    $scope.dorms = [
+      {
+        "name": "Kogalniceanu"
+      },
+      {
+        "name": "Grozavesti"
+      }
     ];
 
-    $scope.todoList.forEach(function (item) {
-      item.color = getRandomColor();
-    });
+    $scope.colleagueOptions = [];
+    
+    $scope.dormOptions = [];
 
-    $scope.newTodoText = '';
+    $scope.newOption = '';
 
-    $scope.addToDoItem = function (event, clickPlus) {
-      if (clickPlus || event.which === 13) {
-        $scope.todoList.unshift({
-          text: $scope.newTodoText,
-          color: getRandomColor(),
-        });
-        $scope.newTodoText = '';
+    $scope.addDormOption = function (event, clickPlus) {
+      if (vm.newOption == undefined) {
+        toastr.error("Nu ati selectat niciun camin");
+      }
+      else {
+        if (clickPlus || event.which === 13) {
+          var obj = $filter('filter')($scope.dorms, { name: vm.newOption }, true)[0];
+          var index = $scope.dorms.indexOf(obj);
+          if (index == -1) {
+            toastr.error("Nu ati selectat niciun camin");
+          }
+          else {
+            $scope.dormOptions.unshift({
+              name: vm.newOption,
+            });
+            $scope.dorms.splice(index, 1);
+            $scope.newOption = '';
+          }
+        }
       }
     };
+
+
+    $scope.deleteOption = function (option) {
+      var index = $scope.dormOptions.indexOf(option);
+      $scope.dormOptions.splice(index, 1);
+      $scope.dorms.push(option);
+    };
+
+    $scope.addColleague = function (event, clickPlus) {
+      if (clickPlus || event.which === 13) {
+        $scope.colleagueOptions.push({
+          name: vm.newColleague,
+        });
+        $scope.newColleague = '';
+      }
+    };
+
+    $scope.deleteColleague = function (option) {
+      var index = $scope.colleagueOptions.indexOf(option);
+      $scope.colleagueOptions.splice(index, 1);
+    };
+
+    $scope.test = function () {
+      debugger;
+      alert($scope.dorms);
+      alert($scope.dormOptions);
+    }
 
   }
 })();
